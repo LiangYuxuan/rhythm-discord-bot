a06cde8d0706b22872a0182c2c2dd71461855288
LiangYuxuan <lyx@lyx.es>
2023-10-17 17:58:14 +0800

    feat: pino -> winston

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,22 @@
-import pino from 'pino';
+import winston from 'winston';
 
-const logger = pino({
-    transport: {
-        target: 'pino-pretty',
-    },
+export default winston.createLogger({
+    format: winston.format.combine(
+        winston.format.errors({
+            stack: false,
+        }),
+        winston.format.splat(),
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss.SSS ZZ',
+        }),
+    ),
+    transports: [
+        new winston.transports.Console({
+            level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`),
+            ),
+        }),
+    ],
 });
-
-export default logger;
