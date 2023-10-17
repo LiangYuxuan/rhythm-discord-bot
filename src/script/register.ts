424a81194d2226028aec6dca8f7e57d10323482c
LiangYuxuan <lyx@lyx.es>
2023-10-17 17:54:14 +0800

    refactor: new rules and tweak

--- src/script/register.ts
+++ src/script/register.ts
@@ -1,69 +1,59 @@
+/* eslint-disable no-console */
+
+import assert from 'node:assert';
+
+import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
-import dotenv from 'dotenv';
-import { exit } from 'process';
 import yargs from 'yargs';
-import logger from '../logger.js';
-import commands from '../discord/commands/index.js';
 
-import type { Command } from '../discord/commands/index';
-
-dotenv.config();
+import commands, { Command } from '../discord/commands/index.ts';
 
 const token = process.env.TOKEN;
 const clientID = process.env.CLIENT_ID;
 const guildID = process.env.GUILD_ID;
-if (token === undefined) {
-    logger.fatal('Missing TOKEN for Discord Token in .env.');
-    exit(1);
-}
-if (clientID === undefined) {
-    logger.fatal('Missing CLIENT_ID for Discord Application ID in .env.');
-    exit(1);
-}
 
-const rest = new REST({ version: '9' }).setToken(token);
+assert(token !== undefined, 'Missing TOKEN for Discord Token');
+assert(clientID !== undefined, 'Missing CLIENT_ID for Discord Application ID');
 
 yargs(process.argv.slice(2))
     .command(['register', '$0'], 'register slash commands', () => {
         // do nothing
     }, async () => {
         try {
-            logger.info('Started refreshing application (/) commands.');
+            const rest = new REST({ version: '9' }).setToken(token);
 
-            let route: `/${string}` = '/';
-            if (guildID === undefined) {
-                route = Routes.applicationCommands(clientID);
-            } else {
-                route = Routes.applicationGuildCommands(clientID, guildID);
-            }
+            console.info('Started refreshing application (/) commands.');
+
+            const route = guildID
+                ? Routes.applicationGuildCommands(clientID, guildID)
+                : Routes.applicationCommands(clientID);
 
             const payload: Command['data'][] = [...commands.values()].map((value) => value.data);
 
             await rest.put(route, { body: payload });
 
-            logger.info('Successfully reloaded application (/) commands.');
+            console.info('Successfully reloaded application (/) commands.');
         } catch (error) {
-            logger.error('%o', error);
+            console.error(error);
         }
     })
     .command('clear', 'clear slash commands', () => {
         // do nothing
     }, async () => {
         try {
-            logger.info('Started clearing application (/) commands.');
+            const rest = new REST({ version: '9' }).setToken(token);
 
-            let route: `/${string}` = '/';
-            if (guildID === undefined) {
-                route = Routes.applicationCommands(clientID);
-            } else {
-                route = Routes.applicationGuildCommands(clientID, guildID);
-            }
+            console.info('Started clearing application (/) commands.');
+
+            const route = guildID
+                ? Routes.applicationGuildCommands(clientID, guildID)
+                : Routes.applicationCommands(clientID);
 
             await rest.put(route, { body: [] });
 
-            logger.info('Successfully cleared application (/) commands.');
+            console.info('Successfully cleared application (/) commands.');
         } catch (error) {
-            logger.error('%o', error);
+            console.error(error);
         }
     })
     .help('h')
